656cf59481aeed8210f28ad2dfdb3ed379673872
your usernamenonodnod <phorton@softstackfactory.com>
2019-04-02 02:14:36 +0000

    mapforloop

---
+++ arrayMap.js
@@ -0,0 +1,21 @@
+const Animals = [    
+	{name: "Fluffykins" , species: "rabbit" },
+	{name: "Caro" , species: "dog" },
+	{name: "Hamilton" , species: "dog"},
+	{name: "Harold", species: "fish"},
+	{name: "Ursula" , species: "cat"},
+	{name: "Jimmy", species: "fish"}
+];
+
+const names = function(arr) {
+    let animalNames = [ ]
+    for(let i = 0; i < arr.length; i++){
+        
+        animalNames.push(arr[i].name)
+       
+    } return animalNames
+    
+}
+
+const namesAnimals = names(Animals)
+console.log(namesAnimals)
